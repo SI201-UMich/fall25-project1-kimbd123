a3178559cfe8c4cfc1ec7768a00da4fa82162a95
Brandon Kim <kimbd@umich.edu>
2025-10-15 00:27:33 -0400

    tests for one function done

--- project_1.py
+++ project_1.py
@@ -41,55 +41,75 @@ def filter_adelie(penguins):
                  adelie_penguins.append(penguin)
       return adelie_penguins
 
-def adelie_flippers(adelie_penguins):
-     adelie_flippers = []
+def avg_adelie_flipper(adelie_penguins):
+     flippers = []
      for penguin in adelie_penguins:
-          flipper_length = penguin["flipper_length_mm"].strip().lower()
+          flipper_length = penguin.get("flipper_length_mm", "")
           if flipper_length and flipper_length.upper() != "NA":
-               adelie_flippers.append(float(flipper_length))
-     return adelie_flippers
+               flippers.append(float(flipper_length))
+     if not flippers:
+            return None
+     return sum(flippers) / len(flippers)
 
-def average_flipper(adelie_flippers):
-     avg = sum(adelie_flippers) / len(adelie_flippers)
-     return avg
-
-def adelie_bodymass(adelie_penguins):
+def max_adelie_bodymass(adelie_penguins):
      masses = []
      for penguin in adelie_penguins:
-          body_mass = penguin["body_mass_g"].strip().lower()
+          body_mass = penguin["body_mass_g"]
           if body_mass and body_mass.upper() != "NA":
                masses.append(float(body_mass))
-     return masses
-
-def max_bodymass(masses):
-     max_adelie = max(masses)
-     return max_adelie
+     return max(masses)
 
 def main():
      penguins = load_penguins('penguins.csv')
      adelie_penguins = filter_adelie(penguins)
-     flippers = adelie_flippers(adelie_penguins)
-     avg_flipper = average_flipper(flippers)
-     bodymasses = adelie_bodymass(adelie_penguins)
-     maximum = max_bodymass(bodymasses)
-     print(avg_flipper)
+     avg = avg_adelie_flipper(adelie_penguins)
+     maximum = max_adelie_bodymass(adelie_penguins)
+     print(avg)
      print(maximum)
      with open("penguins_results.txt", "w") as f:
           f.write("Penguin Report\n")
-          f.write(f"Average flipper length in milimeters for Adelie penguins is {avg_flipper}\n")
+          f.write(f"Average flipper length in milimeters for Adelie penguins is {avg}\n")
           f.write(f"Maximum mass in grams for Adelie penguins is {maximum}")
-     class TestPenguinCalculations(unittest.TestCase):
-            def test1_average_flipper_normal(self):
-                  flippers = ['181', '186', '195']
-                  self.AssertAlmostEqual(average_flipper(flippers), 187.33)
 
-            def test2_average_flipper_normal(self):
-                  flippers = ['193', '190', '186']
-                  self.assertAlmostEqual(average_flipper(flippers), 189.67)
+class TestPenguinCalculations(unittest.TestCase):
+      def test1_average_flipper_normal(self):
+            flippers = [
+                 {"flipper_length_mm": "181"},
+                 {"flipper_length_mm": "186"},
+                  {"flipper_length_mm": "195"},
+                  {"sex": "male"}
+            ]
+            self.assertAlmostEqual(avg_adelie_flipper(flippers), 187.33, places = 2)
 
+      def test2_average_flipper_normal(self):
+            flippers = [
+                 {"flipper_length_mm": "193"},
+                 {"flipper_length_mm": "190"},
+                 {"flipper_length_mm": "186"},
+                 {"bill_length_mm": "36.7"}
+            ]
+            self.assertAlmostEqual(avg_adelie_flipper(flippers), 189.67, places = 2)
+      
+      def test1_average_flipper_edge(self):
+           flippers = [
+                {"flipper_length_mm": "NA"},
+                {"bill_depth_mm": "NA"},
+                {"flipper_length_mm": "190"}
+           ]
+           self.assertEqual(avg_adelie_flipper(flippers), 190)
 
+      def test2_average_flipper_edge(self):
+           flippers = [
+                {"flipper_length_mm": "NA"},
+                {"bill_depth_mm": "NA"},
+                {"flipper_length_mm": "NA"} 
+           ]
+           self.assertEqual(avg_adelie_flipper(flippers), None)
+           
+unittest.main()
 
-main()
+           
+
 
 
           
